f71381279f62813ac1d0fb735d41b43d1a8d561f
Samuel Wright <destiny1new@hotmail.com>
2025-11-06 21:26:07 +0000

    Added fizzbuzz

--- chapter-03/overflow/Program.cs
+++ chapter-03/overflow/Program.cs
@@ -1,4 +1,5 @@
-﻿OverFlow();
+﻿// OverFlow();
+FizzBuzz();
 
 // function for catching an overflow of a byte in a for loop
 void OverFlow()
@@ -30,4 +31,37 @@ void OverFlow()
     {
         Console.WriteLine($"OVERFLOW EXPECTION CAUGHT: {e}");
     }
+}
+
+// function for implementing the childs game fizzbuzz
+// divisible by 3 & 5 fizzbuzz
+// divisible by 3 fizz
+// divisible by 5 buzz
+// neither return number
+void FizzBuzz()
+{
+    // can change this easily in for loop without, easier for other ppl to use this
+    int max = 100;
+    List<string> answer = new List<string>();
+    for (int i = 1; i <= max; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            answer.Add("FizzBuzz");
+        }
+        else if (i % 3 == 0)
+        {
+            answer.Add("Fizz");
+        }
+        else if (i % 5 == 0)
+        {
+            answer.Add("Buzz");
+        }
+        else
+        {
+            answer.Add($"{i}");
+        }
+    }
+    // prints each item of list in a single string spaced with comma + space
+    Console.WriteLine(string.Join(", ", answer));
 }
