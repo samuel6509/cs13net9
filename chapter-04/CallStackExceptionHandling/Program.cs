acf4214fe97fb5eb874cf9d0b8a5531bef44b141
Sam Wright <destiny1new@hotmail.com>
2026-01-07 19:15:13 +0000

    Understanding the call stack

---
+++ chapter-04/CallStackExceptionHandling/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+using CallStackExceptionHandlingLib; // To use Processor. using static System.Console;
+WriteLine("In Main");
+
+Alpha();
+void Alpha()
+{
+  WriteLine("In Alpha");
+  Beta();
+}
+
+void Beta() {
+  WriteLine("In Beta");
+  Processor.Gamma();
+}
