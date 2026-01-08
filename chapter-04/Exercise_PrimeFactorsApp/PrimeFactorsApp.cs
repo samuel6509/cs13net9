6be0d294dae62b8bab6c9b609a1ccdf8fc570903
Sam Wright <destiny1new@hotmail.com>
2026-01-08 14:21:09 +0000

    Progress made on the console app

--- chapter-04/Exercise_PrimeFactorsApp/PrimeFactorsApp.cs
+++ chapter-04/Exercise_PrimeFactorsApp/PrimeFactorsApp.cs
@@ -1,15 +1,34 @@
 ﻿using Exercise_PrimeFactorsLib;
 
-Console.Write("Pick a number between 1 & 1000");
-string input = Console.ReadLine();
+PrimeFactorApp app = new();
+app.App();
 
 class PrimeFactorApp
 {
-    private string App()
+    public string App()
     {
-        Console.Write("Pick a number between 1 & 1000");
-        string input = Console.ReadLine();
-
+        int number;
+        while(true)
+        {
+            Console.Write("Pick a number between 1 & 1000: ");
+            string input = Console.ReadLine();
+            if(string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Try again...");
+                continue;
+            }
+            if(!int.TryParse(input, out number))
+            {
+                Console.WriteLine("Must be a number...");
+                continue;
+            }
+            if(number < 1 || number > 1000)
+            {
+                Console.WriteLine("Number must be between 1 & 1000");
+                continue;
+            }
+            break;
+        }
         return "";
     }
 }
