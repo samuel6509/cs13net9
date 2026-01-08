f9623ce95f40edcd4e8cac9e1106a01f57e49977
Sam Wright <destiny1new@hotmail.com>
2026-01-08 12:42:36 +0000

    Test successful

--- chapter-04/Exercise_PrimeFactorsLib/PrimeFactors.cs
+++ chapter-04/Exercise_PrimeFactorsLib/PrimeFactors.cs
@@ -2,18 +2,18 @@
 
 namespace Exercise_PrimeFactorsLib;
 
-static class PrimeFactor
+public static class PrimeFactor
 {
     // numbers < 1000
     // PRIME FACTORS OF 25
     // 2 / 25 = 50
     // 5 / 5 = 25
     // RETURN 5x5x2
-    static string PrimeFactors(int n)
+    public static string PrimeFactors(int n)
     {
         int current = n;
         var list = new List<int>();
-        for(int i = 2; i < n; i++)
+        for(int i = 2; i <= n; i++)
         {
             while(current % i == 0)
             {
--- chapter-04/Exercise_PrimeFactorsTests/PrimeFactorUnitTests.cs
+++ chapter-04/Exercise_PrimeFactorsTests/PrimeFactorUnitTests.cs
@@ -1,9 +1,17 @@
 ﻿using Exercise_PrimeFactorsLib;
 
-public class UnitTest1
+public class PrimeFactorUnitTests
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(50, "5x5x2")]
+    [InlineData(1000, "5x5x5x2x2x2")]
+    [InlineData(11, "11")]
+    [InlineData(789, "6x7")] // made to fail
+    [InlineData(789, "263x3")]
+    public void TestPrimeFactors(int a, string epxected)
     {
+        string actual = PrimeFactor.PrimeFactors(a);
+        // compare results
+        Assert.Equal(epxected, actual);
     }
 }
