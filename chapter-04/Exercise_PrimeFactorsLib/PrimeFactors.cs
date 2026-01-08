f55174cb3104b9555c7d0ec3e5cb2ae693a953e3
Sam Wright <destiny1new@hotmail.com>
2026-01-08 11:54:19 +0000

    Moving onto unit test

--- chapter-04/Exercise_PrimeFactorsLib/PrimeFactors.cs
+++ chapter-04/Exercise_PrimeFactorsLib/PrimeFactors.cs
@@ -5,9 +5,24 @@ namespace Exercise_PrimeFactorsLib;
 static class PrimeFactor
 {
     // numbers < 1000
+    // PRIME FACTORS OF 25
+    // 2 / 25 = 50
+    // 5 / 5 = 25
+    // RETURN 5x5x2
     static string PrimeFactors(int n)
     {
-        var sb = new StringBuilder();
-        return ""; // placeholder
+        int current = n;
+        var list = new List<string>();
+        for(int i = 2; i < n; i++)
+        {
+            while(current % i == 0)
+            {
+                current /= i;
+                list.Add(i.ToString());
+            }
+        }
+        string answer = string.Join('+', list);
+        Console.WriteLine(answer);
+        return answer;
     }
 }
