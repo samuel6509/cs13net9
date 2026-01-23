1ce0cfee9875e3b5bf758fb16c71e2e3dc229843
Sam Wright <destiny1new@hotmail.com>
2026-01-23 14:46:20 +0000

    learnt about properties using getter setter

--- chapter-05/PacktLibraryNet2/PersonAutoGen.cs
+++ chapter-05/PacktLibraryNet2/PersonAutoGen.cs
@@ -1,2 +1,48 @@
 namespace Packt.Shared;
 // This file simulates an auto-generated class.
+public partial class Person
+{
+    #region Properties: Methods to get and/or set data or state.
+    // A readonly property defined using C# 1 to 5 syntax.
+    public string Origin
+    {
+        get
+        {
+            return string.Format("{0} was born on {1}."
+            ,arg0: Name, arg1: HomePlanet);
+        }
+    }
+    // Two readonly properties defined using C# 6 or later
+    // lambda expression body syntax.
+    public string Greeting => $"{Name} says 'Hello!'";
+    public int Age => DateTime.Today.Year - Born.Year;
+
+    // A read-write property defined using C# 3 auto-syntax.
+    public string? FavoriteIceCream { get; set; }
+    // A private backing field to store the property value.
+    private string? _favoritePrimaryColor;
+
+    // A public property to read and write to the field.
+    public string? FavoritePrimaryColor
+    {
+        get
+        {
+            return _favoritePrimaryColor;
+        }
+        set
+        {
+            switch (value?.ToLower())
+            {
+                case "red":
+                case "green":
+                case "blue":
+                _favoritePrimaryColor = value;
+                break;
+                default:
+                throw new ArgumentException($"{value} is not a primary color. " +
+                                            "Choose from: red, green, blue.");
+            }   
+        }
+    }
+    #endregion
+}
